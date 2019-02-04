912815037e932291ffac64f0cdcd2bfdcbe4fccf
Mattias Karlsson <matkar@wcom.se>
2019-02-05 00:30:40 +0100

    Added BalanceToString !

---
+++ src/Bank/Extensions/AccountExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Bank.Extensions
+{
+    public static class AccountExtensions
+    {
+        public static string BalanceToString(this IAccount account)
+            => FormattableString.Invariant($"{account?.Balance:C}");
+    }
+}
