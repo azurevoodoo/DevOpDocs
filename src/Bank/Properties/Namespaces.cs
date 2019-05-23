7b28a748acad9407928d79eaa015ea103b121bb5
Mattias Karlsson <matkar@wcom.se>
2019-05-23 12:18:11 +0200

    Added namespace docs

---
+++ src/Bank/Properties/Namespaces.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+// ReSharper disable once CheckNamespace
+namespace Bank
+{
+    /// <summary>
+    /// The <see cref="Bank"/> namespace contains fundamental classes and
+    /// base classes for Bank class library.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
