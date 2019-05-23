af513fe88a86f580907605b73a12a08bc087b0f0
Mattias Karlsson <matkar@wcom.se>
2019-05-23 13:31:59 +0200

    Added docs

--- src/Bank/Extensions/AccountExtensions.cs
+++ src/Bank/Extensions/AccountExtensions.cs
@@ -3,6 +3,11 @@ namespace Bank.Extensions
 {
     public static class AccountExtensions
     {
+        /// <summary>
+        /// Balances to string.
+        /// </summary>
+        /// <returns>The to string.</returns>
+        /// <param name="account">Account.</param>
         public static string BalanceToString(this IAccount account)
             => FormattableString.Invariant($"{account?.Balance:C}");
     }
